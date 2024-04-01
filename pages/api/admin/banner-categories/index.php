0be9b7f1b9a0859a304b81b2ca4c11472d304f10
twinh <twinhuang@qq.com>
2024-04-01 21:48:11 +0800

    refactor(banner): 移除测试语句

--- pages/api/admin/banner-categories/index.php
+++ pages/api/admin/banner-categories/index.php
@@ -7,10 +7,4 @@ use Miaoxing\Services\Page\PostToPatchTrait;
 return new class () extends BasePage {
     use CollGetTrait;
     use PostToPatchTrait;
-
-    public function get()
-    {
-        sleep(3);
-        return \Miaoxing\Services\Service\IndexAction::exec($this);
-    }
 };
