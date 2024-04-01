549a906e84e9f5b064753645426e931adf3eea4c
twinh <twinhuang@qq.com>
2024-04-01 20:45:42 +0800

    ci: 更新依赖

--- pages/api/admin/banner-categories/index.php
+++ pages/api/admin/banner-categories/index.php
@@ -7,4 +7,10 @@ use Miaoxing\Services\Page\PostToPatchTrait;
 return new class () extends BasePage {
     use CollGetTrait;
     use PostToPatchTrait;
+
+    public function get()
+    {
+        sleep(3);
+        return \Miaoxing\Services\Service\IndexAction::exec($this);
+    }
 };
