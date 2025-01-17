a4e69c3e81037ce2992d568cb02d08bad5be24c1
twinh <twinhuang@qq.com>
2025-01-17 16:43:33 +0800

    refactor(internal): 生成元数据到模型类中，删除 Trait

--- src/Service/BannerCategoryModel.php
+++ src/Service/BannerCategoryModel.php
@@ -2,7 +2,6 @@
 
 namespace Miaoxing\Banner\Service;
 
-use Miaoxing\Banner\Metadata\BannerCategoryTrait;
 use Miaoxing\Plugin\BaseModel;
 use Miaoxing\Plugin\Model\HasAppIdTrait;
 use Miaoxing\Plugin\Model\ModelTrait;
@@ -10,9 +9,23 @@ use Miaoxing\Plugin\Model\ReqQueryTrait;
 use Miaoxing\Plugin\Model\SnowflakeTrait;
 use Wei\Model\SoftDeleteTrait;
 
+/**
+ * @property string|null $id
+ * @property string $appId 应用编号
+ * @property string $name 名称
+ * @property string $code 标识
+ * @property bool $isBuiltIn 是否内置
+ * @property string $remark 备注
+ * @property array $configs 配置
+ * @property string|null $createdAt
+ * @property string|null $updatedAt
+ * @property string $createdBy
+ * @property string $updatedBy
+ * @property string|null $deletedAt
+ * @property string $deletedBy
+ */
 class BannerCategoryModel extends BaseModel
 {
-    use BannerCategoryTrait;
     use HasAppIdTrait;
     use ModelTrait;
     use ReqQueryTrait;
